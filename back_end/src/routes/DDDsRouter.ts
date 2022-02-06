fd60623cd4976cd1a9cd46a187ce70e77094ab6a
Hamander <1hamander@gmail.com>
2022-02-06 18:33:56 -0300

    build: route create DDD && Schema

--- back_end/src/routes/DDDsRouter.ts
+++ back_end/src/routes/DDDsRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import CreateDDDsController from "../controllers/DDDsController";
+import DDDsValidations from "../validations/DDDsValidations"
+
 
 const routes = Router()
 
-routes.post("/handle", new CreateDDDsController().handle);
+routes.post("/handle", DDDsValidations, new CreateDDDsController().handle);
 
 export default routes
