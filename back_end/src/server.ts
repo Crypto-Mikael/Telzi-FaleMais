2c4a8bb2420914fcce23366fe536d78eae59ebf3
Hamander <1hamander@gmail.com>
2022-02-07 16:18:30 -0300

    refac: routes prefix fixed

--- back_end/src/server.ts
+++ back_end/src/server.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import express from "express";
 import "./database";
-import routes from "./routes/DDDsRouter";
+import routes from "./routes";
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
 app.use(express.json());
 
-app.use('/', routes);
+app.use("/DDDs", routes.DDDs);
 
 app.get("/", (_req: Request, res: Response) => res.send("Hello World!"));
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
