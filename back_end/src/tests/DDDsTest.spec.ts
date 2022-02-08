6b689b500ed242770b3f10ae7fb16fdcab6d75e6
Hamander <1hamander@gmail.com>
2022-02-08 17:15:18 -0300

    build: change name test file && finalize DDDsTest

--- back_end/src/tests/DDDsTest.spec.ts
+++ back_end/src/tests/DDDsTest.spec.ts
@@ -4,11 +4,9 @@ import request from "supertest";
 import { createConnection } from "typeorm";
 import { defaultAPI, addedAPI, updatedAPI } from "./mocks/MocksDDDs";
 
-beforeAll(async () => {
-  await createConnection();
-});
+beforeAll(() => createConnection());
 
-describe("GET / DDDs - Retrive all DDDs", () => {
+describe("GET / DDDs - Get all DDDs", () => {
   it("Should GET all DDDs", async () => {
     const result = await request(server).get("/DDDs");
     expect(result.text).toEqual(JSON.stringify(defaultAPI));
@@ -37,5 +35,18 @@ describe("PUT / DDDs - Update DDD", () => {
       .send({ description: "020" });
     const responseAfter = await request(server).get("/DDDs");
     expect(responseAfter.text).toEqual(JSON.stringify(updatedAPI));
+  });
+});
+
+describe("DELETE / DDDs - Delete DDD", () => {
+  it("Should DELETE one DDD", async () => {
+    const responseBefore = await request(server).get("/DDDs");
+    expect(responseBefore.text).toEqual(JSON.stringify(updatedAPI));
+
+    await request(server).delete(
+      `/DDDs/${updatedAPI[updatedAPI.length - 1].id_DDDs}`
+    );
+    const responseAfter = await request(server).get("/DDDs");
+    expect(responseAfter.text).toEqual(JSON.stringify(defaultAPI));
   });
 });
