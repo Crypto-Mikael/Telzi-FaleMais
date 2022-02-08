c856a0712152163910d90723921a7a4ca0fea768
Hamander <1hamander@gmail.com>
2022-02-08 19:43:24 -0300

    build: OriginDestinyController created

--- back_end/src/controllers/OriginDestinyController.ts
+++ back_end/src/controllers/OriginDestinyController.ts
@@ -1,1 +1,19 @@
 import { Request, Response } from "express";
+import OriginDestinyService from "../service/OriginDestinyService";
+
+export default class OriginDestinyController {
+  async createOriginDestiny(req: Request, res: Response) {
+    const { origin, destiny, value } = req.body;
+    
+    const service = new OriginDestinyService();
+
+    const newOriginDestiny = await service.serviceCreate({ origin, destiny, value });
+
+    if (newOriginDestiny instanceof Error) {
+      const { message } = newOriginDestiny;
+      return res.status(400).json({ message });
+    }
+
+    return res.status(201).json(newOriginDestiny);
+  }
+}
