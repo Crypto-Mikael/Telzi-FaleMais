d920bf8b4793d8f5a6f465bf5cf2faf0b6e1227c
Hamander <1hamander@gmail.com>
2022-02-08 19:47:01 -0300

    build: OriginDestiny Route Created

--- back_end/src/routes/OriginDestinyRouter.ts
+++ back_end/src/routes/OriginDestinyRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import OriginDestinyController from "../controllers/DDDsController";
+import OriginDestinyController from "../controllers/OriginDestinyController";
 import OriginDestinyValidations from "../validations/DDDsValidations";
 
 const routes = Router();
+
+routes.post("/", new OriginDestinyController().createOriginDestiny);
 
 export default routes;
